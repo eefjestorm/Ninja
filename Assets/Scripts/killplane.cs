6a13958c689ba0d19365725b014cc83b7c958400
eefjestorm <elineboekweit@gmail.com>
2016-02-15 12:07:17 +0100

    initial ninja
    initial ninja game

---
+++ Assets/Scripts/killplane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killplane : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            c.GetComponent<Entity>().TakeDamage(10);
+            Debug.Log("you died");
+        }
+    }
+
+
+
+
+}
